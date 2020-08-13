842494337edbbf827e0dce5da07b53512e8f64b0
Henrique Kalife <henrique.kalife@ilegra.com>
2020-08-13 16:43:59 -0300

    feat(submit): implement ui

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return <div className="card text-center">
+    <img src={ avatar_url } alt="" className="round-img" style={{ width: '60px' }}></img>
+    <h3>{ login }</h3>
 
-    return <div className="card text-center">
-      <img src={ avatar_url } alt="" className="round-img" style={{ width: '60px' }}></img>
-      <h3>{ login }</h3>
-
-      <div>
-        <a href={ html_url } className="btn btn-dark btn-sm my-1">
-          More
-        </a>
-      </div>
-    </div>;
-  }
+    <div>
+      <a href={ html_url } className="btn btn-dark btn-sm my-1">
+        More
+      </a>
+    </div>
+  </div>;
 }
 
-export default UserItem
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+}
+
+export default UserItem;
